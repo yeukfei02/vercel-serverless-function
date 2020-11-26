02b327f768979fbb2ca43b8f61f4d4b41a25e5a9
Donald Wu <yeukfei02@gmail.com>
2020-11-26 23:54:57 +0800

    refactor: 💡 remove not used db and lib

--- api/member/create-member.ts
+++ api/member/create-member.ts
@@ -1,35 +1,11 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import { v4 as uuidv4 } from 'uuid';
-import { getDB } from '../../common/common';
 
-const db = getDB();
-
-export default (req: NowRequest, res: NowResponse) => {
+export default (req: NowRequest, res: NowResponse): void => {
   const body = req.body;
 
   if (body) {
     const name = body.name;
     if (name) {
-      const memberListStr = db.getItem('member');
-      if (memberListStr) {
-        const memberList = JSON.parse(memberListStr);
-        const obj = {
-          id: uuidv4(),
-          name: name,
-        };
-        memberList.push(obj);
-
-        db.setItem('member', JSON.stringify(memberList));
-      } else {
-        const memberList = [];
-        const obj = {
-          id: uuidv4(),
-          name: name,
-        };
-        memberList.push(obj);
-        db.setItem('member', JSON.stringify(memberList));
-      }
-
       res.status(201).json({
         message: 'create member',
         name: name,
--- common/common.ts
+++
@@ -1,7 +0,0 @@
-import localStorage from 'node-localstorage';
-const LocalStorage = localStorage.LocalStorage;
-const db = new LocalStorage('./db');
-
-export const getDB = (): localStorage.LocalStorage => {
-  return db;
-};
