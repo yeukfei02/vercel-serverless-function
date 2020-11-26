f4943cbb2ed0c17d3112be70b3484452c3001a8b
Donald Wu <yeukfei02@gmail.com>
2020-11-26 23:23:14 +0800

    feat: 🎸 add vercel serverless func

---
+++ api/member/[id].ts
@@ -0,0 +1,25 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import { getDB } from '../../common/common';
+
+const db = getDB();
+
+export default (req: NowRequest, res: NowResponse) => {
+  const id = req.query.id;
+
+  if (id) {
+    const memberListStr = db.getItem('member');
+    if (memberListStr) {
+      const memberList = JSON.parse(memberListStr);
+      if (memberList) {
+        const member = memberList.filter((item: any, i: number) => {
+          return item.id === id;
+        });
+
+        res.status(200).json({
+          message: 'get member by id',
+          member: member[0],
+        });
+      }
+    }
+  }
+};
